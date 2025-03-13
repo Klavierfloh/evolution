55ec7474ce65d28676ebec6c39bfb1badad79286
Klavierfloh <grund.georg@web.de>
2025-03-13 23:27:19 +0100

    v.0.1. added flags

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ vector<tuple<float, int>> gene(int howMuch)
     allGenes.reserve(howMuch);
 
     uniform_real_distribution<float> dist(0.0, 1.0); // Weight
-    uniform_int_distribution<int> dust(0, 1000);     // Fitness
+    uniform_int_distribution<int> dust(0, 100);     // Fitness
     for (int i = 0; i < howMuch; i++)
     {
         tuple<float, int> oneGene = make_tuple(dist(gen) * 10, dust(gen));
@@ -27,12 +27,15 @@ vector<tuple<float, int>> gene(int howMuch)
     return allGenes;
 }
 
-const int amountOfGenes = 100;
-const int amountOfIndividuals = 100;
+int amountOfGenes = 100;
+int amountOfIndividuals = 100;
 const vector<tuple<float, int>> alleGene = gene(amountOfGenes); // gewicht , Fitnessboost
-const int mutationChance = 7;                                   // in %
-const float maxWeight = 2 * amountOfGenes;
-const float probDiOfWeigth = 0.999;
+int mutationChance = 7;                                         // in %
+int weigtFactor = 2;
+float maxWeight = weigtFactor * amountOfGenes;
+float probDiOfWeigth = 1; // 0.999;
+int amountOfGenerations = 0;
+bool setAmountByFlag = false;
 
 string erzeugeIndividuum(int zahl)
 {
@@ -99,18 +102,18 @@ tuple<int, string> maxFitness(const vector<string> &population)
 
 float weight(const string &individuum)
 {
-    float weight = 0.0;
+    float totalWeight = 0.0;
     for (int i = 0; i < individuum.size(); i++)
     {
         if (individuum[i] == '1')
         {
-            int fit = get<0>(alleGene[i]);
-            weight += fit;
+            float fit = get<0>(alleGene[i]); // Extract as float
+            totalWeight += fit;
         }
     }
-
-    return weight;
+    return totalWeight;
 }
+
 
 void weightClenser(vector<string> &population)
 {
@@ -266,76 +269,198 @@ void printHelp()
          << " --help       Shows this list" << endl;
 }
 
+void printInfo()
+{
+    cout << "This is an implementation of a heuristic algorithm for evolution modeling.\n"
+         << "It is build of the ideas presented here: \n https://inf-schule.de/algorithmen/komplexitaet/rucksackproblem/station_genetischeralgorithmus\n"
+         << "\n In short: \n"
+         << "Every time the simulation starts a list with \"genes\" are generatet.\n"
+         << "These are tuples consisting of a \"weight\" and a \"fitness bonus\". \n"
+         << "At the start an initial population is generated. Therefore the \"genome\" of an indiviual is respresented by a binary string where each digit stands for one of the possible genes (generated previouly) respectively.\n"
+         << "If the indiviual has the gene (1) the fitness boost and the weight of the gene are added to the individual.\n"
+         << "To generate the next generation new indiviuals get generated until the predefined population size is reached.\n"
+         << "The generation of new ones is quite simple. To simulate the passing of genes from parents to children the binary strings of both parents get chopped of at a random index"
+         << " then the parts get puzzled together again to produce two new new genomes that represent two new individuals.\n"
+         << "The selection of the parents is also random. Out of the last population two get chosen and whoever has the highest fitness gets to be the parent. This happens for two parents.\n"
+         << "After the creation of the new indiviuals there is a chance that they mutate. This is implemented so that one random digit of their genome gets flipped.\n"
+         << "To simulate some kind of pressure to adapt to there is the weight. \n"
+         << "Each time before the next generation gets \"born\" every individual that has a higher weight than the maximum weight has a chance to die.\n"
+         << "To disable the selection through weight one can simply set the probability of that happening to zero.\n"
+         << "This way genomes that have higher fitness and lower weight are being preferred/achieved." << endl;
+}
+
 int main(int argc, char *argv[])
 {
     bool loadingBar = false;
     for (int i = 1; i < argc; ++i)
     {
         std::string arg = argv[i];
-        if (arg == "--help")
+        if (arg == "--help" || arg == "-h")
         {
-            printHelp();
+            cout << "Options:\n"
+                 << " --loading            Enables loading bar\n"
+                 << " --generations=N      Sets number of generations\n"
+                 << " --population=N       Sets population size (default: " << amountOfIndividuals << "\n"
+                 << " --mutation=N         Sets mutation chance (in %, default:" << mutationChance << ")\n"
+                 << " --genes=N            Sets the amount of genes (default: " << amountOfGenes << ")\n"
+                 << " --maxWeight=N        Sets max weight of indiviuum (in N times amount of genes, default: " << weigtFactor << ")\n"
+                 << " --chanceWeight=N     Sets the chance of dying of over weight (between 0 and 1, default: " << probDiOfWeigth << ")\n"
+                 << " --info               What does this programm do? \n"
+                 << " --help               Shows this list" << endl;
             return 0;
         }
         else if (arg == "--loading")
         {
             loadingBar = true;
         }
+        else if (arg.rfind("--generations=", 0) == 0) // Starts with "--generations="
+        {
+            string value = arg.substr(14); // Extracts value after "="
+            try
+            {
+                amountOfGenerations = stoi(value);
+                setAmountByFlag = true;
+            }
+            catch (invalid_argument &)
+            {
+                cerr << "Invalid number for generations: " << value << endl;
+                return 1;
+            }
+        }
+        else if (arg.rfind("--population=", 0) == 0) // Starts with "--populaton="
+        {
+            string value = arg.substr(14); // Extracts value after "="
+            try
+            {
+                amountOfIndividuals = stoi(value);
+            }
+            catch (invalid_argument &)
+            {
+                cerr << "Invalid number for population size: " << value << endl;
+                return 1;
+            }
+        }
+        else if (arg.rfind("--mutation=", 0) == 0) // Starts with "--mutation="
+        {
+            string value = arg.substr(11); // Extracts value after "="
+            try
+            {
+                mutationChance = stoi(value);
+            }
+            catch (invalid_argument &)
+            {
+                cerr << "Invalid number for mutation chance: " << value << endl;
+                return 1;
+            }
+        }
+        else if (arg.rfind("--genes=", 0) == 0) // Starts With "--genges"
+        {
+            string value = arg.substr(8);
+            try
+            {
+                amountOfGenes = stoi(value);
+                maxWeight = weigtFactor * amountOfGenes;
+            }
+            catch (invalid_argument &)
+            {
+                cerr << "Invalid number for gene amount: " << value << endl;
+            }
+        }
+        else if (arg.rfind("--maxWeight=", 0) == 0) // Start with "--maxWeight"
+        {
+            string value = arg.substr(12);
+            try
+            {
+                weigtFactor = stoi(value);
+                maxWeight = weigtFactor * amountOfGenes;
+            }
+            catch (invalid_argument &)
+            {
+                cerr << "Invalid argument for weight factor: " << value << endl;
+            }
+        }
+        else if (arg.rfind("--chanceWeight=", 0) == 0)
+        {
+            string value = arg.substr(15);
+            try
+            {
+                probDiOfWeigth = stoi(value);
+            }
+            catch (invalid_argument &)
+            {
+                cerr << "Invalid argument for over Weight: " << value << endl;
+            }
+        }
+        else if (arg == "--info"){
+            printInfo();
+            return 0;}
         else
         {
             printHelp();
-            return 0;
+            cerr << "Unknown option: " << arg << endl;
+            return 1;
         }
     }
 
-    int amount;
-
-    cout << "How many generations? : ";
-    cin >> amount;
-
-    vector<string> pop = simulateGenerations({"NULL"}, amount / 100);
-    printMaxOfPop(pop);
-
-    if (loadingBar)
+    while (true)
     {
-        int done = 0;
-        int barWidth = 70;
-        int toDo = max(1, amount / 100);
-        float progress = 0.0;
-
-        while (done < amount)
+        if (setAmountByFlag == false)
         {
-            pop = simulateGenerations(pop, toDo);
-            done += toDo;
-
-            if (done > amount)
-                done = amount;
-
-            progress = (float)done / amount; // FIX: Correct float division
-
-            // Loading bar display
-            std::cout << "[";
-            int pos = barWidth * progress;
-            for (int i = 0; i < barWidth; ++i)
-            {
-                if (i < pos)
-                    std::cout << "=";
-                else if (i == pos)
-                    std::cout << ">";
-                else
-                    std::cout << " ";
-            }
-            std::cout << "] " << int(progress * 100.0) << " %\r";
-            std::cout.flush();
+            amountOfGenerations = 0;
+            cout << "How many generations? : ";
+            cin >> amountOfGenerations;
         }
-        std::cout << endl; // Finish line break
+        else
+            cout << "To compute " << amountOfGenerations << " generations press ENTER" << endl;
+        string blank = " ";
+        cin.get();
 
+        vector<string> pop = simulateGenerations({"NULL"}, amountOfGenerations / 100);
         printMaxOfPop(pop);
-    }
-    else
-    {
-        vector<string> pop = simulateGenerations({"NULL"}, amount);
-        printMaxOfPop(pop);
+
+        if (loadingBar)
+        {
+            int done = 0;
+            int barWidth = 70;
+            int toDo = max(1, amountOfGenerations / 100);
+            float progress = 0.0;
+
+            while (done < amountOfGenerations)
+            {
+                pop = simulateGenerations(pop, toDo);
+                done += toDo;
+
+                if (done > amountOfGenerations)
+                    done = amountOfGenerations;
+
+                progress = (float)done / amountOfGenerations; // FIX: Correct float division
+
+                // Loading bar display
+                std::cout << "[";
+                int pos = barWidth * progress;
+                for (int i = 0; i < barWidth; ++i)
+                {
+                    if (i < pos)
+                        std::cout << "=";
+                    else if (i == pos)
+                        std::cout << ">";
+                    else
+                        std::cout << " ";
+                }
+                std::cout << "] \033[33m" << int(progress * 100.0) << "\033[0m %\r";
+                std::cout.flush();
+            }
+            std::cout << endl; // Finish line break
+
+            printMaxOfPop(pop);
+        }
+        else
+        {
+            vector<string> pop = simulateGenerations({"NULL"}, amountOfGenerations);
+            printMaxOfPop(pop);
+        }
+        cout << "\n \033[32m <===========================================> \033[0m\n"
+             << endl;
     }
 
     return 0;
